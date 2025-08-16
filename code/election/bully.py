a7f3c4635631ac0f8a333651e286a1c8e7ee2412
MocDenny <denise.comincioli@studenti.unitn.it>
2025-08-16 09:31:52 +0200

    added timestamps + concurrent initiators option

--- code/election/bully.py
+++ code/election/bully.py
@@ -17,7 +17,13 @@ class BullySimulation:
             self.nodes[i].obtain_peers(self.nodes)
 
     # method to start an election
-    def start_election(self):
+    def start_election(self, n_initiators = 1):
+        # reject operation if initiators are too many
+        if n_initiators > (len(self.nodes) - 1):
+            print("\031[1;94m------------------------------------------------\n")
+            print("OPERATION REJECTED: number of initiators greater than non-coordinator nodes!\033[0m\n")
+            return
+
         # restore all nodes default status
         for i in range(len(self.nodes)):
             self.nodes[i].reset()
@@ -25,27 +31,32 @@ class BullySimulation:
         self.finish_event = self.env.event()
         self.add_triggers()
 
-        print("------------------------------------------------\n")
-        print("Starting Bully election algorithm\n")
+        print("\033[1;94m------------------------------------------------\n")
+        print("Start Bully election algorithm\033[0m\n")
         
         # coordinator (crashed)
         self.nodes[len(self.nodes)-1].crash()
         
-        initiator = self.nodes[random.randint(len(self.nodes))]
-        # if the initiator is crashed choose another one
-        while initiator.crashed :
-            initiator = self.nodes[random.randint(len(self.nodes))]
-            
-        # create the election message 
+        # there can be multiple nodes that detect coordinator crash
+        initiators = []
+        for i in range(n_initiators):
+            init = self.nodes[random.randint(len(self.nodes))]
+            # if generated initiator is the crashed coordinator, generate a new one
+            while init.crashed or init in initiators:
+                init = self.nodes[random.randint(len(self.nodes))]
+
+            initiators.append(init)
+
+        # all initiators start election
         election_msg = BullyMsg("ELECTION", -1)
-        # starts election
-        yield initiator.queue.put(election_msg) 
-        self.env.process(initiator.receive())
+        for i in range(n_initiators):
+            yield initiators[i].queue.put(election_msg) 
+            self.env.process(initiators[i].receive())
 
         # wait for finish_event to be triggered (means that election ended)
         yield self.finish_event
-        print("\nBully election algorithm terminated")
-        print("\n------------------------------------------------\n")
+        print("\n\033[1;94mBully election algorithm terminated")
+        print("\n------------------------------------------------\033[0m\n")
 
     def add_triggers(self):
         for i in range(len(self.nodes)):
--- code/node/bully_node.py
+++ code/node/bully_node.py
@@ -1,3 +1,4 @@
+import numpy
 from node.node import Node
 from msg.bully_msg import BullyMsg
 
@@ -5,62 +6,72 @@ from msg.bully_msg import BullyMsg
 class BullyNode(Node):
 
     def __init__(self, env, id):
-       super().__init__(env, id)
-       self.is_waiting = False
-       self.blocked = False
+        super().__init__(env, id)
+        self.elected = -1
+        self.el_in_progress = False
+        self.blocked = False
 
     def send(self, type, dest_id):
         if not self.peers[dest_id].crashed:
-            # create the election message 
+            # create the election message
             election_msg = BullyMsg(type, self.id)
 
             # DEBUG
-            print(f"Node {self.id} sends {type} to node {dest_id}")
-            
-            yield self.env.timeout(20)
-            # send the message 
-            yield self.peers[dest_id].queue.put(election_msg) 
-            self.env.process(self.peers[dest_id].receive())
+            print(f"Time {self.env.now:.2f}: Node {self.id} sends {type} to node {dest_id}")
 
+            yield self.env.timeout(numpy.random.uniform(0.1, 10))
+            # send the message
+            yield self.peers[dest_id].queue.put(election_msg)
+            self.env.process(self.peers[dest_id].receive())
 
     def receive(self):
         msg = yield self.queue.get()
         if msg.type == "ELECTION":
             if msg.sender_id == -1:
                 # start election
-                print(f"Node {self.id} detected coordinator crash")
-            else:    
+                print(f"\033[94mTime {self.env.now:.2f}: Node {self.id} detected coordinator crash\033[0m")
+            else:
                 # DEBUG
-                print(f"Node {self.id} receives ELECTION message from node {msg.sender_id}")
+                print(
+                    f"Time {self.env.now:.2f}: Node {self.id} receives ELECTION message from node {msg.sender_id}"
+                )
 
             if self.id > msg.sender_id:
                 if msg.sender_id != -1:
                     # if id is greater than sender: stop election
                     self.env.process(self.send("OK", msg.sender_id))
-                if not self.is_waiting:
-                    # only start election ONCE
-                    self.is_waiting = True
+                if not self.el_in_progress:
+                    # each node can only start election ONCE
+                    self.el_in_progress = True
                     # send ELECTION messages to all nodes with greater id
                     for i in range(self.id + 1, len(self.peers)):
                         self.env.process(self.send("ELECTION", i))
+                        
                     # TO BE CHANGED: works in synchronous systems where you have an upper bound of network delays
-                    yield self.env.timeout(40 * len(self.peers) + 1)
-                    self.is_waiting = False
-                    #   wait for a certain time to be passed ... if it wasn't stopped it is elected
+                    yield self.env.timeout(20 * (len(self.peers) - self.id - 1))
+
+                    # wait for a certain time to be passed ... if it wasn't stopped it is elected
                     if not self.blocked:
                         for i in range(len(self.peers)):
-                            self.env.process(self.send("COORDINATOR", self.peers[i].id))
+                            self.env.process(
+                                self.send("COORDINATOR", self.peers[i].id))
+                    # election of node ended
+                    self.el_in_progress = False
 
         elif msg.type == "OK":
             # DEBUG
-            print(f"Node {self.id} receives OK message from node {msg.sender_id}")
+            print(
+                f"Time {self.env.now:.2f}: Node {self.id} receives OK message from node {msg.sender_id}"
+            )
             # stop election
             self.blocked = True
 
         elif msg.type == "COORDINATOR":
             # DEBUG
-            print(f"Node {self.id} elects {msg.sender_id} as coordinator")
+            print(f"\033[92mTime {self.env.now:.2f}: Node {self.id} elects {msg.sender_id} as coordinator\033[0m")
             self.elected = msg.sender_id
+            # if active, set election status of node
+            self.el_in_progress = False
             # if election terminated trigger finish event
             if self.finished():
                 self.finish.succeed()
@@ -68,12 +79,13 @@ class BullyNode(Node):
     # returns True if all nodes decided on coordinator
     def finished(self):
         for i in range(len(self.peers)):
-            if self.peers[i].elected == -1 and not self.peers[i].crashed:     # count only non crashed processes
+            if self.peers[i].elected == -1 and not self.peers[i].crashed:  # count only non crashed processes
                 return False
-        return True      
+        return True
 
+    # resets node to default status
     def reset(self):
         self.blocked = False
         self.crashed = False
         self.elected = -1
-        self.is_waiting = False      
+        self.el_in_progress = False
